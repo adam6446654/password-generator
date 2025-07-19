8d57d204b5e3b32e2c63dad6fc551d5c7b86c51f
adam6446654 <adamshahid20060000@gmail.com>
2025-07-19 12:37:40 +0500

    fixes

--- passgen.py
+++ passgen.py
@@ -3,7 +3,6 @@ import random
 def user_input_validation():
     while True:
         pass_1 = True
-        pass_2 = True
         try:
             length = int(input("Password Length: "))
             while length < 0 or length > 16:
@@ -14,17 +13,28 @@ def user_input_validation():
         except:
             pass_1 = False
             print("Entered invalid data type. try again.")
+            continue
 
+        if pass_1 == True:
+            break
+    
+    while True:
+        pass_2 = True
         try:
             temp = input("Special Characters? (1 (yes) / 0 (no)): ")
+            if int(temp) != 1 and int(temp) != 0:
+                raise Exception("Input must be 0 or 1")
             use_special_char = bool(int(temp))
             pass_2 = True
         except:
             pass_2 = False
             print("entered invalid option. Try again.")
+
+        if pass_2 == True:
+            break
         
-        if pass_1 == True and pass_2 == True:
-            return length,use_special_char
+    if pass_1 == True and pass_2 == True:
+        return length,use_special_char
 
 def random_pass_gen(length,use_special_char):
     password = ""
@@ -38,3 +48,11 @@ def random_pass_gen(length,use_special_char):
                 password += chr(temp)
     
     return password
+
+def main():
+    a,b = user_input_validation()
+    password = random_pass_gen(a,b)
+    print(password)
+
+if __name__ == "__main__":
+    main()
